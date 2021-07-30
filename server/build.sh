00905b7340753a19e73758adac68e51e8c367199
yxf12213090 <yxf12213090@126.com>
2021-07-30 13:28:29 +0800

    build

--- server/build.sh
+++ server/build.sh
@@ -4,11 +4,13 @@ set -e
 APP_NAME="demoapp"
 APP_PATH="/home/work/app/pythonapp"
 #拷贝代码文件
-cp -r server APP_PATH/${APP_NAME}
-cd APP_PATH/${APP_NAME}
+cp -r ../server/* ${APP_PATH}/${APP_NAME}
+cd ${APP_PATH}/${APP_NAME}
 
 python -m venv env
 source env/bin/activate
-pip3 config set global.index-url https://mirrors.aliyun.com/pypi/simple
-pip3 install -r requirements1.txt
+pip install --upgrade pip
+pip install -r requirements1.txt
+chmod 775 supervisor.conf
 supervisord -c supervisor.conf
+echo "finished"
